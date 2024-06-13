9bd542c02a32499688d0dc75a0c3667e18ce6bc2
Steve <83976005+Steven-Dawson18@users.noreply.github.com>
2024-06-13 20:30:48 +0100

    added axios for product detail

--- client/src/app/layouts/header.tsx
+++ client/src/app/layouts/header.tsx
@@ -1,5 +1,29 @@
-import { DarkMode } from "@mui/icons-material";
-import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+const midLinks = [
+    { title: 'catalog', path: '/catalog' },
+    { title: 'about', path: '/about' },
+    {title: 'contact', path: '/contact'},
+]
+
+const rightLinks = [
+    { title: 'login', path: '/login' },
+    { title: 'register', path: '/register' },
+]
+
+const navStyles = {
+                                color: 'inherit',
+                                textDecoration: 'none',
+                                typography: 'h6',
+                                '&:hover': {
+                                    color: 'grey.500'
+                                },
+                                '&.avtive': {
+                                    color: 'text.secondary'
+                                }
+                            }
 
 interface Props {
     darkMode: boolean;
@@ -9,11 +33,54 @@ interface Props {
 export default function Header({darkMode, handleThemeChange}: Props) {
     return (
         <AppBar position="static" sx={{mb: 4}}>
-            <Toolbar>
-                <Typography variant="h6">
+            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                
+                <Box display='flex' alignItems='center'>
+                    <Typography variant="h6" component={NavLink}
+                    to="/"
+                    sx={navStyles}
+                    >
                     Van Store
-                </Typography>
-                <Switch checked={darkMode} color="info" onChange={handleThemeChange}/>
+                    </Typography>
+                    <Switch checked={darkMode} color="info" onChange={handleThemeChange} />
+                </Box>
+
+                <List sx={{ display: 'flex' }}>
+                    {midLinks.map(({title, path }) => (
+                        <ListItem
+                            component={NavLink}
+                            to={path}
+                            key={path}
+                            sx={navStyles}
+                        >
+                        
+                            {title.toLocaleUpperCase()}
+                        </ListItem>
+                    ))}
+                </List>
+
+                <Box display='flex' alignItems='center'>
+                    <IconButton size='large' edge='start' color="inherit" sx={{ mr: 2 }}>
+                    <Badge badgeContent="4" color="secondary">
+                        <ShoppingCart />
+                    </Badge>
+                    </IconButton>
+
+                    <List sx={{ display: 'flex' }}>
+                        {rightLinks.map(({title, path }) => (
+                            <ListItem
+                                component={NavLink}
+                                to={path}
+                                key={path}
+                                sx={navStyles}
+                            >
+                            
+                                {title.toLocaleUpperCase()}
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+                
             </Toolbar>
         </AppBar>
     )
